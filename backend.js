29ad833081abdcabf7d13bb18e1c8f7d15249bde
msalim5 <ashazsalim7@gmail.com>
2025-03-22 09:34:02 -0500

    Server setup

--- backend.js
+++ backend.js
@@ -1,1 +1,12 @@
-console.log("Hello")
+const exp = require('constants');
+const express = require('express');
+const app = express();
+const path = require('path')
+
+app.get('/about', (req, res) => {
+  res.status(200).send('About Page');
+})
+
+app.listen(5500, ()=>{
+  console.log('Server is listening on port 5500');
+})
